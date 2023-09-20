86edd9b3267b1ed45a5ff0a1caf89f36fc18d83f
jeffreyhallett <31424548+jeffreyhallett@users.noreply.github.com>
2023-09-20 16:38:25 -0500

    finished

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,10 +1,29 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter the number you would like to sieve up to");
+		int n = in.nextInt();
+		Boolean[] arrayA = new Boolean[n];
+		for (int i = 0; i < n; i++) {
+			arrayA[i] = true;
+		}
+		for (int i = 2; i < Math.sqrt(n); i++) {
+			if (arrayA[i] == true) {
+				for (int j = (int) Math.pow((double) i,2.0); j < n; j = j + i) {
+					arrayA[j] = false;
+				}
+			}
+		}
+		for (int i = 2; i < n; i++) {
+			if (arrayA[i] == true) {
+				System.out.println(i + " ");
+			}
+		}
 	}
 
 }
